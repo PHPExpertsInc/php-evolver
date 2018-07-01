4e29aa8cf1bcf281e5036dea94b461d8e0fadc8c
Peter Coles <peter@kuroiweb.com>
2018-07-01 14:27:54 +0100

    Initial commit. Package skeleton with big hat tip to Spatie upon whose skeleton this is based.

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+All notable changes to `petercoles\gao` will be documented in this file
+
+## 1.0.0 - 2018-07-31
+
+- initial release (target)
---
+++ src/GAOServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace PeterColes\GAO;
+
+use Illuminate\Support\ServiceProvider;
+
+class GAOServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+        //
+    }
+}
