ccae18c4257a22040f58c38f2105e7e367627145
Peter Coles <peter@kuroiweb.com>
2018-07-06 10:02:56 +0100

    Data manager for commonly-need tasks when working with large evaluation datasets.

---
+++ src/DataManager.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace PeterColes\GAO;
+
+use DirectoryIterator;
+use Illuminate\Support\Collection;
+
+class DataManager
+{
+    public function loadCsvDir(String $inDir): Collection
+    {
+        $allData = [];
+        foreach (new DirectoryIterator($inDir) as $fileInfo) {
+            if (!$fileInfo->isDot()) {
+                $allData[] = $this->csvToArray($fileInfo->getPathname());
+            }
+        }
+
+        return collect($allData);
+    }
+
+    public function split(Collection $data, Float $testDataRatio): Collection
+    {
+        return $data->partition(function () use ($testDataRatio) {
+            return mt_rand() / mt_getrandmax() > $testDataRatio;
+        });
+    }
+
+    public function save(String $outFile, Collection $contents)
+    {
+        file_put_contents($outFile, json_encode($contents->values()));
+    }
+
+    public function load(String $inFile): Collection
+    {
+        return collect(json_decode(file_get_contents($inFile)));
+    }
+
+    public function setMemoryLimit($limit = '1G')
+    {
+        ini_set('memory_limit', $limit);
+    }
+
+    protected function csvToArray(String $filepath, Bool $headings = false): array
+    {
+        $rows = array_map('str_getcsv', file($filepath));
+
+        if ($headings) {
+            array_shift($rows);
+        }
+
+        return $rows;
+    }
+}
---
+++ tests/DataManagerTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace PeterColes\GAO\Tests;
+
+use PHPUnit\Framework\TestCase;
+use PeterColes\GAO\DataManager;
+
+class DataManagerTest extends TestCase
+{
+    protected $dm;
+
+    protected $racesFile = __DIR__ . '/data/races.json';
+
+    public function setUp()
+    {
+        $this->dm = new DataManager();
+    }
+
+    public function tearDown()
+    {
+        if (file_exists($this->racesFile)) {
+            unlink($this->racesFile);
+        }
+    }
+
+    /** @test */
+    public function can_load_csv_files_from_directory()
+    {
+        $data = $this->dm->loadCsvDir(__DIR__ . '/data/races');
+
+        $this->assertCount(10, $data);
+        $this->assertCount(36, $data[3]);
+        $this->assertCount(2, $data[7][21]);
+    }
+
+    /** @test */
+    public function can_split_collection()
+    {
+        mt_srand(42);
+
+        $data = $this->dm->loadCsvDir(__DIR__ . '/data/races');
+        $split = $this->dm->split($data, .2);
+
+        $this->assertCount(8, $split->first());
+        $this->assertCount(2, $split->last());
+    }
+
+    /** @test */
+    public function can_read_and_write_data_as_json()
+    {
+        $this->assertFileNotExists($this->racesFile);
+
+        $data = $this->dm->loadCsvDir(__DIR__ . '/data/races');
+
+        $this->dm->save($this->racesFile, $data);
+
+        $this->assertJsonFileEqualsJsonFile(__DIR__ . '/data/expected-races.json', $this->racesFile);
+        $retrievedData = $this->dm->load($this->racesFile);
+
+        $this->assertEquals($data, $retrievedData);
+    }
+
+    /** @test */
+    public function can_change_memory_limit()
+    {
+        $initialLimit = ini_get('memory_limit');
+
+        $newLimit = $initialLimit == '512M' ? '1G' : '512M';
+
+        $this->dm->setMemoryLimit($newLimit);
+
+        $this->assertEquals($newLimit, ini_get('memory_limit'));
+    }
+}
